96f653a8855ebb3fec33ac4f3395b5a91236b4ef
Steven Rojas <stvrjs.k12@gmail.com>
2018-11-20 22:22:35 -0600

    Continuacion del aplicativo

--- src/app/components/template/template.component.ts
+++ src/app/components/template/template.component.ts
@@ -8,14 +8,38 @@ import { NgForm } from '@angular/forms';
 })
 export class TemplateComponent implements OnInit {
 
+  usuario: Object = {
+    nombre: null,
+    apellido: null,
+    correo: null,
+    pais: "",
+    sexo: "Hombre",
+    acepta: false
+  };
+
+  paises = [ {
+    codigo: 'CRI',
+    nombre: 'Costa Rica'
+  },
+  {
+    codigo: 'ESP',
+      nombre: 'Espana'
+  }];
+
+  sexos: string[] = ['Hombre', 'Mujer', 'Sin definir'];
+
   constructor() { }
 
   ngOnInit() {
   }
 
   guardar(forma: NgForm) {
-    console.log('Posteado');
-    console.log(forma);
+
+    console.log('ngForm', forma);
+    console.log('Valor', forma.value);
+
+    console.log('usuario', this.usuario);
+
   }
 
 }
